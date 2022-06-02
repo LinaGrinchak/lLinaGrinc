06c977ea5209c4c2f4a8c07655375c67bf89362f
LinaGrinchak <106731272+LinaGrinchak@users.noreply.github.com>
2022-06-02 16:15:02 +0700

    Update TextAutomaticallyEditor.java

--- GSO_81/src/main/java/gso81/TextAutomaticallyEditor.java
+++ GSO_81/src/main/java/gso81/TextAutomaticallyEditor.java
@@ -9,7 +9,7 @@ public class TextAutomaticallyEditor {
         if (str != null && !str.isEmpty()) {
             for (int i = 0; i < str.length(); i++)
                 if (isLetterOrDigit(str.charAt(i))) {
-                    builder.append(toUpperCase(str.charAt(i))).append(str, i + 1, str.length());
+                    builder.append(toUpperCase(str.charAt(i))).append(str, ++i, str.length());
                     break;
                 } else {
                     builder.append(str.charAt(i));
@@ -27,6 +27,27 @@ public class TextAutomaticallyEditor {
         } else return false;
     }
 
+    String alphabetize(String str) {
+        if (str != null && !str.isEmpty()) {
+            str = leaveOnlyLatinLetters(str);
+            String lettersUpperCase = sortedLetters(leaveUpperCaseLatinLetters(str));
+            if (!str.isEmpty()) {
+                String sortedStr = sortedLetters(str.toLowerCase());
+                if (!lettersUpperCase.isEmpty()) {
+                    StringBuilder result = new StringBuilder(str.length());
+                    for (int i = 0; i < lettersUpperCase.length(); i++) {
+                        int pos = sortedStr.indexOf(toLowerCase(lettersUpperCase.charAt(i)));
+                        sortedStr = result.append(sortedStr, 0, pos)
+                                .append(lettersUpperCase.charAt(i))
+                                .append(sortedStr.substring(++pos)).toString();
+                        result.setLength(0);
+                    }
+                }
+                return sortedStr;
+            } else return str;
+        } else return str;
+    }
+
     public static String leaveOnlyLetters(String str) {
         StringBuilder result = new StringBuilder(str.length());
         for (int i = 0; i < str.length(); i++) {
@@ -37,24 +58,49 @@ public class TextAutomaticallyEditor {
         }
         return result.toString();
     }
-    String alphabetize(String str) {
-        if (str != null && !str.isEmpty()) {
-            str = str.replaceAll("[^a-zA-Z]", "");
-            String lettersUpperCase = sortedLetters(str.replaceAll("[^A-Z]", ""));
-            if (!str.isEmpty()) {
-                String sortedStr = sortedLetters(str.toLowerCase());
-               if(!lettersUpperCase.isEmpty()){
-                   for (int i = 0; i  < lettersUpperCase.length(); i++){
-                       String letter =String.valueOf(lettersUpperCase.charAt(i));
-                       sortedStr = sortedStr.replaceFirst(letter.toLowerCase(),letter);
-                   }
-               }
-            return sortedStr;
-            } else return str;
-        } else return str;
+
+    public static String leaveOnlyLatinLetters(String str) {
+        StringBuilder result = new StringBuilder(str.length());
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (isLowerCaseLatinLetter(c) || isUpperCaseLatinLetter(c)) {
+                result.append(c);
+            }
+        }
+        return result.toString();
     }
 
-    public static String sortedLetters(String str){
+    public static String leaveLowerCaseLatinLetters(String str) {
+        StringBuilder result = new StringBuilder(str.length());
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (isLowerCaseLatinLetter(c)) {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
+
+    public static String leaveUpperCaseLatinLetters(String str) {
+        StringBuilder result = new StringBuilder(str.length());
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (isUpperCaseLatinLetter(c)) {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
+
+    public static boolean isLowerCaseLatinLetter(char letter) {
+        return ('a' <= letter && letter <= 'z');
+    }
+
+    public static boolean isUpperCaseLatinLetter(char letter) {
+        return ('A' <= letter && letter <= 'Z');
+    }
+
+    public static String sortedLetters(String str) {
         return str.chars()
                 .sorted()
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
