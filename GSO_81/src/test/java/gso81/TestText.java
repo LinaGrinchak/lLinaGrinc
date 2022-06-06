9f00bef3a592a24ae9bb7c321551d3dc7c36beb1
LinaGrinchak <106731272+LinaGrinchak@users.noreply.github.com>
2022-06-06 18:25:02 +0700

    Update TestText.java

--- GSO_81/src/test/java/gso81/TestText.java
+++ GSO_81/src/test/java/gso81/TestText.java
@@ -1,12 +1,10 @@
 package gso81;
 
+import org.junit.Test;
 public class TestText {
-    public static void main(String[] args) throws Exception {
-        doTestCensured();
-        doTestTextAutomaticalllyEditor();
-    }
 
-    private static void doTestCensured() {
+    @Test
+    public void doTestCensured() {
         System.out.println(Censured.class.getName() + ":");
         String censuredWord = "idler";
         String text = "idlerIdleR IdLeRis A IdLeR";
@@ -14,12 +12,14 @@ public class TestText {
         System.out.println("censure: " + censured.censure(text));
     }
 
-    private static void doTestTextAutomaticalllyEditor() {
-        System.out.println(TextAutomaticallyEditor.class.getName() + ":");
+    @Test
+    public  void doTestTextAutomaticalllyEditor() {
+        System.out.println(TextAnalyzer.class.getName() + ":");
         String[] text = new String[]{" $ a 4t gd", "Was it a car or a cat I saw?", "Madam, I’m Adam",
                 "apple", "4 bd", "", null, "Алла",
                 "привет Привет ages 12 through 14 (6-8 Grades) and high school from ages 15 through 19 (9-12 Grades).", "Eve"};
-        TextAutomaticallyEditor textEditor = new TextAutomaticallyEditor();
+        String palindrome = "Was it a car or a cat I saw?";
+        TextAnalyzer textEditor = new TextAnalyzer();
         for (String message : text) {
             System.out.println("isPalindrome: " + message + " " + textEditor.isPalindrome(message));
             System.out.println("capitalize: " + textEditor.capitalize(message));
