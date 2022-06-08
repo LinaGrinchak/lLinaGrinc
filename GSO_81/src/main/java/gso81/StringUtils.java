e39f2668e6f000f79676d6c4cadee768ef7ac1fd
LinaGrinchak <106731272+LinaGrinchak@users.noreply.github.com>
2022-06-08 18:23:45 +0700

    Update and rename TextAnalyzer.java to StringUtils.java

--- GSO_81/src/main/java/gso81/StringUtils.java
+++ GSO_81/src/main/java/gso81/StringUtils.java
@@ -1,11 +1,11 @@
 package gso81;
-
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 import static java.lang.Character.*;
 
-public class TextAnalyzer {
+public class StringUtils {
     public static String capitalize(String str) {
         StringBuilder builder = new StringBuilder();
         if (str != null && !str.isEmpty()) {
@@ -25,17 +25,20 @@ public class TextAnalyzer {
             String onlyLetters = leaveOnlyLetters(str);
             if (!onlyLetters.isEmpty()) {
                 return onlyLetters.equalsIgnoreCase(new StringBuilder(onlyLetters).reverse().toString());
-            } else return false;
-        } else return false;
+            }
+            return false;
+        }
+        return false;
     }
 
     public static String alphabetize(String str) {
         if (str != null && !str.isEmpty()) {
             return sortedLetters(str);
-        } else return str;
+        }
+        return str;
     }
 
-    public static String leaveOnlyLetters(String str) {
+    private static String leaveOnlyLetters(String str) {
         StringBuilder result = new StringBuilder(str.length());
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
@@ -46,8 +49,7 @@ public class TextAnalyzer {
         return result.toString();
     }
 
-
-    public static String leaveLatinLetters(String str) {
+    private static String leaveLatinLetters(String str) {
         StringBuilder result = new StringBuilder(str.length());
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
@@ -58,26 +60,28 @@ public class TextAnalyzer {
         return result.toString();
     }
 
-    public static boolean isLatinLetters(char c) {
+    private static boolean isLatinLetters(char c) {
         return (isLowerCaseLatinLetter(c) || isUpperCaseLatinLetter(c));
     }
 
-    public static boolean isLowerCaseLatinLetter(char letter) {
+    private static boolean isLowerCaseLatinLetter(char letter) {
         return ('a' <= letter && letter <= 'z');
     }
 
-    public static boolean isUpperCaseLatinLetter(char letter) {
+    private static boolean isUpperCaseLatinLetter(char letter) {
         return ('A' <= letter && letter <= 'Z');
     }
 
-    public static String sortedLetters(String str) {
+    private static String sortedLetters(String str) {
         Comparator<String> comparator = (s1, s2) -> {
             int r = s1.compareToIgnoreCase(s2);
             return r == 0 ? s1.compareTo(s2) : r;
         };
-
-        return Arrays.stream(leaveLatinLetters(str).split("")).sorted(comparator)
-                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
-                .toString();
+        List<String> sortedLetters = Arrays.asList(leaveLatinLetters(str).split(""));
+        sortedLetters.sort(comparator);
+        StringBuilder result = new StringBuilder(str.length());
+        for (String s:sortedLetters){
+            result.append(s);}
+        return result.toString();
     }
 }
