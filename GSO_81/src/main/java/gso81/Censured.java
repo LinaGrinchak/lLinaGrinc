d23b6b80a4cbfe755a99a8373d9de3654b8a954d
LinaGrinchak <106731272+LinaGrinchak@users.noreply.github.com>
2022-06-15 17:19:07 +0700

    Update Censured.java

--- GSO_81/src/main/java/gso81/Censured.java
+++ GSO_81/src/main/java/gso81/Censured.java
@@ -2,23 +2,22 @@ package gso81;
 
 import java.util.StringTokenizer;
 
-import static gso81.StringUtils.leaveLettersAndDigit;
+import static gso81.StringUtils.isEmpty;
+import static gso81.StringUtils.leaveLettersAndDigits;
 
 public class Censured {
     private static final String CENSORED = "censored";
     private String censor;
 
     public Censured(String censor) throws Exception {
-        if (censor == null || censor.trim().isEmpty()) {
+        if (isEmpty(censor)) {
             throw new Exception("Censored word empty");
         }
         this.censor = censor;
     }
 
     public String censure(String str) {
-        if (str == null || str.trim().isEmpty()) {
-            return str;
-        } else if (!isWordFitOnTheLine(str)) {
+        if (isEmpty(censor) || !isWordFitOnTheLine(str)) {
             return str;
         } else if (censor.contains(" ") || censor.contains("-")) {
             return censorOnlyWord(str);
@@ -28,14 +27,14 @@ public class Censured {
         while (strTokens.hasMoreTokens()) {
             String currentToken = strTokens.nextToken();
             if (!isWordFitOnTheLine(currentToken)) {
-                result.append(currentToken).append(" ");
+                result.append(currentToken);
             } else if (currentToken.length() == censor.length() && currentToken.equalsIgnoreCase(censor)) {
-                result.append(CENSORED).append(" ");
+                result.append(CENSORED);
             } else {
-                String censWord = censorOnlyWord(currentToken);
-                result.append(censoredWithoutSeparators(censWord)).append(" ");
-
+                String censuredOnlyWord = censorOnlyWord(currentToken);
+                result.append(censorWithoutSeparators(censuredOnlyWord));
             }
+            result.append(" ");
         }
         return result.deleteCharAt(result.length() - 1).toString();
     }
@@ -47,24 +46,21 @@ public class Censured {
         int startStr = 0;
         int censorLength = censor.length();
         StringBuilder builder = new StringBuilder();
-        for (int inxStr = 0; inxStr + censorLength <= str.length(); inxStr++)
+        for (int inxStr = 0; inxStr + censorLength <= str.length(); inxStr++) {
             if (str.substring(inxStr, inxStr + censorLength).equalsIgnoreCase(censor)) {
                 builder.append(str, startStr, inxStr).append(CENSORED);
                 startStr = inxStr + censorLength;
                 inxStr += (censorLength - 1);
             }
+        }
         builder.append(str.substring(startStr));
         return builder.toString();
 
     }
 
-    private String censoredWithoutSeparators(String censWord) {
-        String onlyLettersWord = leaveLettersAndDigit(censWord);
-        if (isContainCensor(onlyLettersWord)) {
-            return censorOnlyWord(onlyLettersWord);
-        } else {
-            return censWord;
-        }
+    private String censorWithoutSeparators(String str) {
+        String lettersAndDigitsWord = leaveLettersAndDigits(str);
+        return isContainCensor(lettersAndDigitsWord) ? censorOnlyWord(lettersAndDigitsWord) : str;
     }
 
     private boolean isWordFitOnTheLine(String str) {
@@ -74,5 +70,4 @@ public class Censured {
     private boolean isContainCensor(String str) {
         return str.toLowerCase().contains(censor.toLowerCase());
     }
-
 }
