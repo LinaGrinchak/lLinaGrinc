fca4ea5f9675d5a1a339da1b96983a94118a7996
LinaGrinchak <106731272+LinaGrinchak@users.noreply.github.com>
2022-06-17 18:51:14 +0700

    Update TestStringUtils.java

--- GSO_81/src/test/java/com/dgphoenix/gso81/TestStringUtils.java
+++ GSO_81/src/test/java/com/dgphoenix/gso81/TestStringUtils.java
@@ -9,65 +9,68 @@ public class TestStringUtils {
 
     @Test
     public void isPalindromeNull() {
-        Assert.assertTrue(!StringUtils.isPalindrome(null));
+        Assert.assertFalse(StringUtils.isPalindrome(null));
     }
 
     @Test
     public void isCapitalizeNull() {
-        Assert.assertTrue(StringUtils.capitalize(null).equals(""));
+        Assert.assertEquals("", StringUtils.capitalize(null));
     }
 
     @Test
     public void isAlphabetizeNull() {
-        Assert.assertTrue(StringUtils.alphabetize(null).equals(""));
+        Assert.assertEquals("", StringUtils.alphabetize(null));
     }
 
     @Test
     public void isPalindromeEmpty() {
-        Assert.assertTrue(!StringUtils.isPalindrome(""));
+        Assert.assertFalse(StringUtils.isPalindrome(""));
     }
 
     @Test
     public void isCapitalizeEmpty() {
-        Assert.assertTrue(StringUtils.capitalize("").equals(""));
+        Assert.assertEquals("", StringUtils.capitalize(""));
 
     }
 
     @Test
     public void isAlphabetizeEmpty() {
-        Assert.assertTrue(StringUtils.alphabetize("").equals(""));
+        Assert.assertEquals("", StringUtils.alphabetize(""));
     }
 
     @Test
     public void isAlphabetizeTextSpaces() {
-        Assert.assertTrue(StringUtils.alphabetize("        ").equals(""));
+        Assert.assertEquals("", StringUtils.alphabetize("        "));
     }
 
     @Test
     public void isPalindromeTextSpaces() {
-        Assert.assertTrue(!StringUtils.isPalindrome("        "));
+        Assert.assertFalse(StringUtils.isPalindrome("        "));
     }
 
     @Test
     public void isCapitalizeTextSpaces() {
-        Assert.assertTrue(StringUtils.capitalize("        ") == "");
+        Assert.assertEquals("", StringUtils.capitalize("        "));
 
     }
 
     @Test
     public void isAlphabetizeMixedText() {
-        Assert.assertTrue(StringUtils.alphabetize(MIXED_TEXT).equals("aaaaacdddeeeefGGggggghhhhhhhilmnooooorrrrrsssssttuu"));
+        String expectedStr = "aaaaacdddeeeefggGgggGhhhhhhhilmnooooorrrrrsssssttuu";
+        
+        Assert.assertEquals(expectedStr, StringUtils.alphabetize(MIXED_TEXT));
     }
 
     @Test
     public void isPalindromeMixedText() {
-        Assert.assertTrue(!StringUtils.isPalindrome(MIXED_TEXT));
+        Assert.assertFalse(StringUtils.isPalindrome(MIXED_TEXT));
     }
 
     @Test
     public void isCapitalizeMixedText() {
-        Assert.assertTrue(StringUtils.capitalize(MIXED_TEXT)
-                .equals(" Привет Привет ages 12 through 14 (6-8 Grades) and high school from ages 15 through 19 (9-12 Grades)."));
+        String expectedStr = " Привет Привет ages 12 through 14 (6-8 Grades) and high school from ages 15 through 19 (9-12 Grades).";
+
+        Assert.assertEquals(expectedStr, StringUtils.capitalize(MIXED_TEXT));
     }
 
     @Test
@@ -79,20 +82,17 @@ public class TestStringUtils {
 
     @Test
     public void isAlphabetizePalindromeText() {
-        String[] alphabetizePalindromeText = {
-                "aadMm", "", "Eev", "", "AAVVvv", "AaBbC",
-        };
-        for (int i=0; i<PALINDROME_TEXTS.length; i++) {
-             Assert.assertTrue(StringUtils.alphabetize(PALINDROME_TEXTS[i]).equals(alphabetizePalindromeText[i]));
+        String[] alphabetizePalindromeText = {"aadMm", "", "Eev", "", "AAVvvV", "aABbC",};
+        
+        for (int i = 0; i < PALINDROME_TEXTS.length; i++) {
+            Assert.assertEquals(alphabetizePalindromeText[i], StringUtils.alphabetize(PALINDROME_TEXTS[i]));
         }
     }
 
     @Test
     public void isCapitalizePalindromeText() {
         for (String message : PALINDROME_TEXTS) {
-            Assert.assertTrue(StringUtils.capitalize(message).equals(message));
+            Assert.assertEquals(message, StringUtils.capitalize(message));
         }
     }
-
-
 }
