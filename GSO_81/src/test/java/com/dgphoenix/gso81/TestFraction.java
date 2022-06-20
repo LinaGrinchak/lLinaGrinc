dd8f41af1d97a03504b5af4c9da4f01f9ddc55be
LinaGrinchak <106731272+LinaGrinchak@users.noreply.github.com>
2022-06-20 18:13:12 +0700

    Update TestFraction.java

--- GSO_81/src/test/java/com/dgphoenix/gso81/TestFraction.java
+++ GSO_81/src/test/java/com/dgphoenix/gso81/TestFraction.java
@@ -1,162 +1,215 @@
 package com.dgphoenix.gso81;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestFraction {
-    String fractionStr1 = "6/8";
-    String fractionStr2 = "-6/57";
-
-    private void createFraction(String fractionStr) {
-        System.out.println(StringUtils.class.getName() + ":");
-        System.out.println("Input fraction: " + fractionStr);
+    @Test
+    public void isNull() {
         try {
-            Fraction fraction = new Fraction(fractionStr);
-            System.out.println("fraction: " + fraction);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void sumFractions(String fractionStr1, String fractionStr2) {
-        System.out.println(StringUtils.class.getName() + ":");
-        System.out.println("Input fraction 1: " + fractionStr1 + " Input fraction 2: " + fractionStr2);
-        try {
-            Fraction fraction1 = new Fraction(fractionStr1);
-            Fraction fraction2 = new Fraction(fractionStr2);
-            System.out.println(fraction1 + "+" + fraction2 + "=" + fraction1.add(fraction2));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void subtractFractions(String fractionStr1, String fractionStr2) {
-        System.out.println(StringUtils.class.getName() + ":");
-        System.out.println("Input fraction 1: " + fractionStr1 + " Input fraction 2: " + fractionStr2);
-        try {
-            Fraction fraction1 = new Fraction(fractionStr1);
-            Fraction fraction2 = new Fraction(fractionStr2);
-            System.out.println(fraction1 + "-" + fraction2 + "=" + fraction1.subtract(fraction2));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void multiplyFractions(String fractionStr1, String fractionStr2) {
-        System.out.println(StringUtils.class.getName() + ":");
-        System.out.println("Input fraction 1: " + fractionStr1 + " Input fraction 2: " + fractionStr2);
-        try {
-            Fraction fraction1 = new Fraction(fractionStr1);
-            Fraction fraction2 = new Fraction(fractionStr2);
-            System.out.println(fraction1 + "*" + fraction2 + "=" + fraction1.multiply(fraction2));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void dividedFractions(String fractionStr1, String fractionStr2) {
-        System.out.println(StringUtils.class.getName() + ":");
-        System.out.println("Input fraction 1: " + fractionStr1 + " Input fraction 2: " + fractionStr2);
-        try {
-            Fraction fraction1 = new Fraction(fractionStr1);
-            Fraction fraction2 = new Fraction(fractionStr2);
-            System.out.println(fraction1 + "/" + fraction2 + "=" + fraction1.divided(fraction2));
-        } catch (Exception e) {
-            e.printStackTrace();
+            Fraction fraction = new Fraction(null);
+            Assert.fail("Expected Exception");
+        } catch (Exception thrown) {
+            Assert.assertEquals(null, thrown.getMessage());
         }
     }
 
     @Test
-    public void doTestInputFraction() {
-        createFraction(fractionStr1);
+    public void isEmpty() {
+        try {
+            Fraction fraction = new Fraction("");
+            Assert.fail("Expected Exception");
+        } catch (Exception thrown) {
+            Assert.assertEquals("Numerator entered incorrectly", thrown.getMessage());
+        }
     }
 
     @Test
-    public void doTestWrongString() {
-        createFraction("$ a 4t gd");
+    public void isAllEmpty() {
+        try {
+            Fraction fraction = new Fraction("/");
+            Assert.fail("Expected Exception");
+        } catch (Exception thrown) {
+            Assert.assertEquals("Fraction entered incorrectly", thrown.getMessage());
+        }
     }
 
     @Test
-    public void doTestWrongNumerator() {
-        createFraction("f/4");
+    public void isNumeratorEmpty() {
+        try {
+            Fraction fraction = new Fraction("/1");
+            Assert.fail("Expected Exception");
+        } catch (Exception thrown) {
+            Assert.assertEquals("Numerator entered incorrectly", thrown.getMessage());
+        }
     }
 
     @Test
-    public void doTestWrongDenominator() {
-        createFraction("1/d");
+    public void isDenominatorEmpty() {
+        try {
+            Fraction fraction = new Fraction("1/");
+        } catch (Exception thrown) {
+            Assert.assertEquals("Denominator entered incorrectly", thrown.getMessage());
+        }
     }
 
     @Test
-    public void doTestInt() {
-        createFraction("5");
+    public void isWrongInputFraction() {
+        try {
+            Fraction fraction = new Fraction("$ a 4t gd");
+            Assert.fail("Expected Exception");
+        } catch (Exception thrown) {
+            Assert.assertEquals("For input string: \"$ a 4t gd\"", thrown.getMessage());
+        }
     }
 
     @Test
-    public void doTestNull() {
-        createFraction(null);
+    public void isWrongNumerator() {
+        try {
+            Fraction fraction = new Fraction("f/4");
+            Assert.fail("Expected Exception");
+        } catch (Exception thrown) {
+            Assert.assertEquals("For input string: \"f\"", thrown.getMessage());
+        }
     }
 
     @Test
-    public void doTestMinusNumerator() {
-        createFraction("-6/5");
+    public void isWrongDenominator() {
+        try {
+            Fraction fraction = new Fraction("1/d");
+            Assert.fail("Expected Exception");
+        } catch (Exception thrown) {
+            Assert.assertEquals("For input string: \"d\"", thrown.getMessage());
+        }
     }
 
     @Test
-    public void doTestMinusDenominator() {
-        createFraction("3/-5");
+    public void isExcessNumerator() {
+        try {
+            Fraction fraction = new Fraction("2147483649/2147483640");
+            Assert.fail("Expected Exception");
+        } catch (Exception thrown) {
+            Assert.assertEquals("For input string: \"2147483649\"", thrown.getMessage());
+        }
     }
 
     @Test
-    public void doTestDoubleMinus() {
-        createFraction("-3/-4");
+    public void isExcessDenominator() {
+        try {
+            Fraction fraction = new Fraction("2147483640/2147483649");
+            Assert.fail("Expected Exception");
+        } catch (Exception thrown) {
+            Assert.assertEquals("For input string: \"2147483649\"", thrown.getMessage());
+        }
+    }
+
+
+    @Test
+    public void isEnteredInteger() throws Exception {
+        String expected = "5/1";
+        Fraction fraction = new Fraction("5");
+        Assert.assertEquals(expected, fraction.toString());
     }
 
     @Test
-    public void doTestAdd() {
-        sumFractions("5/8", "3/12");
+    public void isMinusNumerator() throws Exception {
+        String expected = "-1/3";
+        Fraction fraction = new Fraction("-5/15");
+        Assert.assertEquals(expected, fraction.toString());
     }
 
     @Test
-    public void doTestInputFractionAdd() {
-        sumFractions(fractionStr1, fractionStr2);
+    public void isMinusDenominator() throws Exception {
+        String expected = "-1/3";
+        Fraction fraction = new Fraction("5/-15");
+        Assert.assertEquals(expected, fraction.toString());
     }
 
     @Test
-    public void doTestMultiply() {
-        multiplyFractions("5/8", "11/12");
+    public void isDoubleMinus() throws Exception {
+        String expected = "1/3";
+        Fraction fraction = new Fraction("-5/-15");
+        Assert.assertEquals(expected, fraction.toString());
+    }
+
+
+    @Test
+    public void isAdd() throws Exception {
+        Fraction fraction = new Fraction("-5/30");
+        Fraction fraction2 = new Fraction("5/18");
+        String expected = "1/9";
+        Assert.assertEquals(expected, fraction.add(fraction2).toString());
     }
 
     @Test
-    public void doTestInputMultiply() {
-        multiplyFractions(fractionStr1, fractionStr2);
+    public void isExcessAdd() throws Exception {
+        Fraction fraction = new Fraction("-5/2147483640");
+        Fraction fraction2 = new Fraction("5/18");
+        try {
+            fraction.add(fraction2);
+            Assert.fail("Expected Exception");
+        } catch (Exception thrown) {
+            Assert.assertEquals("Number out of bounds for Integer values", thrown.getMessage());
+        }
     }
 
     @Test
-    public void doTestSubtract() {
-        subtractFractions("5/8", "11/12");
+    public void isSubtract() throws Exception {
+        Fraction fraction = new Fraction("-5/30");
+        Fraction fraction2 = new Fraction("5/18");
+        String expected = "-4/9";
+        Assert.assertEquals(expected, fraction.subtract(fraction2).toString());
     }
 
     @Test
-    public void doTestInputFractionSubtract() {
-        subtractFractions(fractionStr1, fractionStr2);
+    public void isExcessSubtract() throws Exception {
+        Fraction fraction = new Fraction("-5/2147483640");
+        Fraction fraction2 = new Fraction("5/18");
+        try {
+            fraction.subtract(fraction2);
+            Assert.fail("Expected Exception");
+        } catch (Exception thrown) {
+            Assert.assertEquals("Number out of bounds for Integer values", thrown.getMessage());
+        }
     }
 
     @Test
-    public void doTestDivided() {
-        dividedFractions("5/8", "0/12");
+    public void isMultiply() throws Exception {
+        Fraction fraction = new Fraction("-9/30");
+        Fraction fraction2 = new Fraction("5/18");
+        String expected = "-1/12";
+        Assert.assertEquals(expected, fraction.multiply(fraction2).toString());
     }
 
     @Test
-    public void doTestInputFractionDivided() {
-        dividedFractions(fractionStr1, fractionStr2);
+    public void isExcessMultiply() throws Exception {
+        Fraction fraction = new Fraction("-5/2147483640");
+        Fraction fraction2 = new Fraction("5/18");
+        try {
+            fraction.multiply(fraction2);
+            Assert.fail("Expected Exception");
+        } catch (Exception thrown) {
+            Assert.assertEquals("Number out of bounds for Integer values", thrown.getMessage());
+        }
     }
 
     @Test
-    public void doTestSubtractExceeding() {
-        subtractFractions("2147483647/8", "-1000/12");
+    public void isDivided() throws Exception {
+        Fraction fraction = new Fraction("-9/30");
+        Fraction fraction2 = new Fraction("5/18");
+        String expected = "-27/25";
+        Assert.assertEquals(expected, fraction.divided(fraction2).toString());
     }
 
     @Test
-    public void doTestInputFractionExceeding() {
-        dividedFractions("2147483647/8", "2147483647/8");
+    public void isExcessDivided() throws Exception {
+        Fraction fraction = new Fraction("-5/2147483640");
+        Fraction fraction2 = new Fraction("170/18");
+        try {
+            fraction.divided(fraction2);
+            Assert.fail("Expected Exception");
+        } catch (Exception thrown) {
+            Assert.assertEquals("Number out of bounds for Integer values", thrown.getMessage());
+        }
     }
 }
