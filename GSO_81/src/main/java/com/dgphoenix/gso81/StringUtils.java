3bd461021962ffc3fa56d26352e47549156397a2
LinaGrinchak <106731272+LinaGrinchak@users.noreply.github.com>
2022-06-20 15:16:16 +0700

    Update StringUtils.java

--- GSO_81/src/main/java/com/dgphoenix/gso81/StringUtils.java
+++ GSO_81/src/main/java/com/dgphoenix/gso81/StringUtils.java
@@ -10,13 +10,9 @@ public class StringUtils {
         if (isEmpty(str)) {
             return "";
         }
-
-        StringBuilder builder = new StringBuilder();
-        char symbol = str.trim().charAt(0);
-        int posSymbol = str.indexOf(symbol);
-
-        builder.append(str,0,posSymbol).append(toUpperCase(symbol)).append(str, posSymbol+1, str.length());
-
+        StringBuilder builder = new StringBuilder(str.length());
+        char symbol = str.charAt(0);
+        builder.append(toUpperCase(symbol)).append(str,  1, str.length());
         return builder.toString();
     }
 
@@ -24,9 +20,7 @@ public class StringUtils {
         if (isEmpty(str)) {
             return false;
         }
-
-        String  revertStr = new StringBuilder(str.trim()).reverse().toString();
-
+        String revertStr = new StringBuilder(str.trim()).reverse().toString();
         return str.trim().equalsIgnoreCase(revertStr);
     }
 
@@ -38,21 +32,9 @@ public class StringUtils {
         return str == null || str.trim().isEmpty();
     }
 
-    static String leaveLettersAndDigits(String str) {
-        StringBuilder result = new StringBuilder(str.length());
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            if (isLetter(c) || isDigit(c)) {
-                result.append(c);
-            }
-        }
-        return result.toString();
-    }
-
     private static String leaveLatinLetters(String str) {
         StringBuilder result = new StringBuilder(str.length());
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
+        for (char c : str.toCharArray()) {
             if (isLatinLetters(c)) {
                 result.append(c);
             }
@@ -73,20 +55,12 @@ public class StringUtils {
     }
 
     private static String sortLetters(String str) {
-        char [] letChars = str.toCharArray();
-        String[] letters= new String[letChars.length];
-
-        for (int i=0; i<letChars.length; i++){
-           letters[i]= String.valueOf(letChars[i]);
-        }
-
-        Arrays.sort(letters,CASE_INSENSITIVE_ORDER);
-
+        String[] letters =  str.split("");
+        Arrays.sort(letters, CASE_INSENSITIVE_ORDER);
         StringBuilder result = new StringBuilder(str.length());
         for (String s : letters) {
             result.append(s);
         }
-
         return result.toString();
     }
 }
