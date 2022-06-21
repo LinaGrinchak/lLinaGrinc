116ce956ca153240094ce1663b77a6f75a9c711c
LinaGrinchak <106731272+LinaGrinchak@users.noreply.github.com>
2022-06-21 16:58:30 +0700

    Update TemperatureConverterFahrenheit.java

--- GSO_81/src/main/java/com/dgphoenix/gso81/temperature/TemperatureConverterFahrenheit.java
+++ GSO_81/src/main/java/com/dgphoenix/gso81/temperature/TemperatureConverterFahrenheit.java
@@ -5,20 +5,23 @@ public class TemperatureConverterFahrenheit extends TemperatureConverter {
 
     TemperatureConverterFahrenheit() {
     }
+
     public static TemperatureConverter getInstance() {
         return temperatureConverter;
     }
+
     @Override
     public double convertFromCelsius(double C) {
         return C * COEFFICIENT_FAHRENHEIT + POINT_MELTING_C_IN_F;
     }
+
     @Override
     public double convertFromFahrenheit(double F) {
         return F;
     }
+
     @Override
     public double convertFromKelvin(double K) {
         return K * COEFFICIENT_FAHRENHEIT - POINT_MELTING_F_IN_K;
     }
-
 }
