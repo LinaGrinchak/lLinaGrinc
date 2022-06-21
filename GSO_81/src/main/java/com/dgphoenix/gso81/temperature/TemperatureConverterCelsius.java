5d214296058e9609cbacb3e89a3b90d103320ae5
LinaGrinchak <106731272+LinaGrinchak@users.noreply.github.com>
2022-06-21 16:58:56 +0700

    Update TemperatureConverterCelsius.java

--- GSO_81/src/main/java/com/dgphoenix/gso81/temperature/TemperatureConverterCelsius.java
+++ GSO_81/src/main/java/com/dgphoenix/gso81/temperature/TemperatureConverterCelsius.java
@@ -1,7 +1,6 @@
 package com.dgphoenix.gso81.temperature;
 
 public class TemperatureConverterCelsius extends TemperatureConverter {
-
     private static TemperatureConverter temperatureConverter = new TemperatureConverterCelsius();
 
     TemperatureConverterCelsius() {
@@ -18,12 +17,11 @@ public class TemperatureConverterCelsius extends TemperatureConverter {
 
     @Override
     public double convertFromFahrenheit(double F) {
-       return  (F - POINT_MELTING_C_IN_F) / COEFFICIENT_FAHRENHEIT;
+        return (F - POINT_MELTING_C_IN_F) / COEFFICIENT_FAHRENHEIT;
     }
 
     @Override
     public double convertFromKelvin(double K) {
         return K - POINT_FREEZING_OF_WATER_C_IN_K;
     }
-
 }
